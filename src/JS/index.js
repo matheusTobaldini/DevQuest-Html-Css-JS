fc8d6f41a3cecdbcf81123776c720b7a5b2989e5
matheusTobaldini <matheus.tobaldini123@gmail.com>
2025-02-23 16:09:14 -0300

    JS finalizado

--- src/JS/index.js
+++ src/JS/index.js
@@ -2,20 +2,35 @@ const btnEnviar = document.getElementById('buttonEnviar');
 
 const inputs = document.querySelectorAll('.inputForm');
 
-const campoObrigatorio = document.getElementsByClassName('campoObrigatorio');
+const campoObrigatorio = document.querySelectorAll('.campoObrigatorio');
 
-btnEnviar.addEventListener("click", verificarInput())
+console.log(inputs);
+
+
+btnEnviar.addEventListener("click", verificarInput)
 
 function verificarInput() {
     inputs.forEach(function (item) {
-        const inputVazio = inputs.value == null;
+        const inputVazio = item.value === '';
 
         if (inputVazio) {
-            item.classList.add('vazio')
-
+            item.classList.add('vazio');
+            
         } else {
             item.classList.remove('vazio');
+            item.classList.add('preenchido');
         }
     });
-}
 
+    campoObrigatorio.forEach(function(item, index) {
+        const campoInput = inputs[index];
+        
+        const estaVazio = campoInput.classList.contains('vazio');
+
+        if(estaVazio) {
+            item.classList.add('erro');
+        } else {
+            item.classList.remove('erro')
+        }
+    })
+}
