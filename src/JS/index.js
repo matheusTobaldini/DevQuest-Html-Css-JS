4ec15e77a41492b4ea0f42d67c39d41bbf2dcb48
matheusTobaldini <matheus.tobaldini123@gmail.com>
2025-02-25 20:46:26 -0300

    corrigido e comparado com a instrução do professor, faltando apenas o responsivo

--- src/JS/index.js
+++ src/JS/index.js
@@ -4,34 +4,49 @@ const inputs = document.querySelectorAll('.inputForm');
 
 const campoObrigatorio = document.querySelectorAll('.campoObrigatorio');
 
-console.log(inputs);
+btnEnviar.addEventListener("click", (e) => {
+    e.preventDefault()
 
-
-btnEnviar.addEventListener("click", verificarInput)
-
-function verificarInput() {
-    inputs.forEach(function (item) {
-        const inputVazio = item.value === '';
-
-        if (inputVazio) {
-            item.classList.add('vazio');
-            item.classList.remove('preenchido')
-            
+    inputs.forEach(function(input) {   
+                
+        if (input.value) {
+            input.classList.add('preenchido')
+            input.classList.remove('vazio')
+            input.nextElementSibling.classList.remove('erro')
         } else {
-            item.classList.remove('vazio');
-            item.classList.add('preenchido');
-        }
-    });
-
-    campoObrigatorio.forEach(function(item, index) {
-        const campoInput = inputs[index];
-        
-        const estaVazio = campoInput.classList.contains('vazio');
-
-        if(estaVazio) {
-            item.classList.add('erro');
-        } else {
-            item.classList.remove('erro')
+            input.classList.remove('preenchido')
+            input.classList.add('vazio')
+            input.nextElementSibling.classList.add('erro')
         }
     })
-}
+})
+
+// function verificarInput() {
+//     inputs.forEach(function (item) {
+//         const inputVazio = item.value === '';
+
+//         if (inputVazio) {
+//             item.classList.add('vazio');
+//             item.classList.remove('preenchido')
+
+//         } else {
+//             item.classList.remove('vazio');
+//             item.classList.add('preenchido');
+//         }
+//     });
+
+//     campoObrigatorio.forEach(function(item, index) {
+//         const campoInput = inputs[index];
+
+//         const estaVazio = campoInput.classList.contains('vazio');
+
+//         if(estaVazio) {
+//             item.classList.add('erro');
+//         } else {
+//             item.nextElementSibling.remove('erro')
+//         }
+//     })
+
+// }
+
+
